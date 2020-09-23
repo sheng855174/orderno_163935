cea70e81820789bb1a846af4af095ce53a43677c
sheng855174 <sheng855174@gmail.com>
2020-09-23 22:39:38 +0800

    v1.0

--- Main.py
+++ Main.py
@@ -5,9 +5,20 @@ import os
 from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.support.ui import Select
-
-# -*- coding: utf-8 -*-
 import xlrd
+import xlwt
+from xlrd import open_workbook
+from xlutils.copy import copy
+
+def isElementExist(driver,element):
+    flag=True
+    try:
+        driver.find_element_by_xpath(element)
+        return flag
+    except:
+        flag=False
+        return flag
+
 
 file = open("config.txt", "r",encoding="utf-8")
 config = json.loads(file.read())
@@ -17,8 +28,8 @@ print("config.txt 讀取完成.")
 delayTime = config["delayTime"]
 物件追蹤表excel = config["物件追蹤表excel"]
 身分證字號excel = config["身分證字號excel"]
+輸出excel = config["輸出excel"]
 選擇縣市url = config["選擇縣市url"]
-選擇區域url = config["選擇區域url"]
 主頁面url = config["主頁面url"]
 
 
@@ -26,21 +37,24 @@ caps = DesiredCapabilities.INTERNETEXPLORER
 caps['ignoreProtectedModeSettings'] = True
 caps['ignoreZoomSetting'] = True
 
-#driver = webdriver.Edge()
 driver = webdriver.Edge(executable_path='msedgedriver.exe')
 driver.get(主頁面url)
 
 
 data = xlrd.open_workbook(物件追蹤表excel)
 id_excel = xlrd.open_workbook(身分證字號excel)
+workbook = copy(open_workbook(物件追蹤表excel))
+
 data_json = {};
 
 print("請手動登入帳號，完成後按任意鍵繼續.")
 os.system("pause")
+driver.get(選擇縣市url)
 
 #讀取資料表
 for n in range(len(data.sheet_names())):
     table = data.sheets()[n]
+    sheet = workbook.get_sheet(n)
     
     #資料表之中欄位
     if n == 0:
@@ -48,7 +62,7 @@ for n in range(len(data.sheet_names())):
     for i in range(1,table.nrows):
         #取出所有row欄位到data_json
         for j in range(len(table.row_values(i))-1):
-            if table.row_values(i)[j] != "" :
+            if table.row_values(i)[j] != "":
                 data_json[data_row[j]] = table.row_values(i)[j]
         id_table = id_excel.sheets()[i-1]
         身分證字號清單 = []
@@ -56,6 +70,7 @@ for n in range(len(data.sheet_names())):
         for 身分證字號 in 身分證字號清單:
             段建號 = data_json["段建號"]
             #選擇地區
+            time.sleep(delayTime)
             driver.get(選擇縣市url)
             縣市 = 段建號[0:段建號.find("市")+1].replace("\n", "")
             縣市下拉式選單 = Select(driver.find_element_by_id('City_ID'))
@@ -71,12 +86,11 @@ for n in range(len(data.sheet_names())):
             建號 = 段建號[段建號.find("建號")-10:段建號.find("建號")-1]
             地號 = data_json["地號"][-11:-2]
             申請用途 = Select(driver.find_element_by_id('applyfor'))
-            申請用途.select_by_visible_text("其他")
+            申請用途.select_by_visible_text("購屋、貸款使用")
             print("段小段 : " + 段小段)
             print("建號 : " + 建號)
             print("地號 : " + 地號)
             print("身分證字號 : " + 身分證字號)
-            print("==================================================")
             driver.find_element_by_name("MAXPAGE").send_keys("\b\b\b1");
             driver.find_element_by_id("INPUT_011").clear()
             driver.find_element_by_id("INPUT_011").send_keys(段小段)
@@ -88,5 +102,20 @@ for n in range(len(data.sheet_names())):
             time.sleep(delayTime)
             driver.find_element_by_name("btnsend").send_keys(Keys.ENTER)
             time.sleep(delayTime)
-os.system("pause")
+            #檢查失敗 or 成功
+            error = isElementExist(driver,"//*[@id=\"ErrorMsgArea\"]")
+            if error!=True :
+                print("資料正確!寫入excel")
+                sheet.write(i, 42, 身分證字號)
+                workbook.save('output.xls')
+                print("==================================================")
+                break
+            else :
+                print("不正確的資料!!!!!!")
+            print("==================================================")
 
+
+
+print("=========================================================")
+print("執行完畢")
+os.system("pause")
